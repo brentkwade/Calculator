999c2d8c2121dbf1fc9bc876d1013a6ee77e4bce
Brent Wade <amedeod@d150fueling.com>
2019-05-02 09:41:33 -0400

    worked on regFile, still need to finish addSub

--- addSub.vhdl
+++ addSub.vhdl
@@ -2,8 +2,13 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity fullAdder is
-    port(A, B, Cin: in std_logic;
-        S, Cout: out std_logic);
+    port(
+        A: in std_logic;
+        B: in std_logic;
+        Cin: in std_logic;
+        S: out std_logic;
+        Cout: out std_logic
+        );
 end fullAdder;
 
 architecture gateLevel of fullAdder is
