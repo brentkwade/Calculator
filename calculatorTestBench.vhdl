de3511444ea36cd7b985e0bb05c68d7c1d7b1a39
Michael Schwartz <michaelschwartz@Michaels-MacBook-Pro.local>
2019-05-04 12:02:35 -0400

    updated ports in testbench and added test file

--- calculatorTestBench.vhdl
+++ calculatorTestBench.vhdl
@@ -3,40 +3,40 @@ use ieee.std_logic_1164.all;
 use std.textio.all;
 
 
-entity calculator_tb is
-end entity calculator_tb;
+entity calculatorTestBench is
+end entity calculatorTestBench;
 
-architecture structural of calculator_tb is
+architecture structural of calculatorTestBench is
 
 component calculator is
   port(
     I : in std_logic_vector(7 downto 0); 
-    clk : in std_logic
+    clock : in std_logic
   );
 end component calculator;
 
 signal I : std_logic_vector(7 downto 0);
-signal clk : std_logic;
+signal clock : std_logic;
 
 begin
-    calculator_0 : calculator port map(I, clk);
+    calculator_0 : calculator port map(I, clock);
 
     process
-      file instruction_file : text is in "instructions.txt"; 
-      variable instruction_line : line;
-      variable intruction_vector : bit_vector(7 downto 0);
+      file testFile : text is in "calculatorTest.txt"; 
+      variable testLine : line;
+      variable testVector : bit_vector(7 downto 0);
     begin
       wait for 999 ps; 
-      while (not(endfile(instruction_file))) loop 
-        clk <= '0';
+      while (not(endfile(testFile))) loop 
+        clock <= '0';
 
-        readline(instruction_file, instruction_line); 
-        read(instruction_line, intruction_vector); 
-        I <= to_stdlogicvector(intruction_vector); 
+        readline(testFile, testLine); 
+        read(testLine, testVector); 
+        I <= to_stdlogicvector(testVector); 
 
         
         wait for 1 ns;
-        clk <= '1';
+        clock <= '1';
         wait for 1 ns;
       end loop;
       wait;
