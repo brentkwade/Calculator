cc3f7ceb791bcfc5c8b76873be8d49233db1ee41
Michael Schwartz <michaelschwartz@Michaels-MacBook-Pro.local>
2019-05-04 20:42:36 -0400

    clockOutput almost done

--- clockOutput.vhdl
+++ clockOutput.vhdl
@@ -46,49 +46,49 @@ end architecture structural;
 
 
 --D Flip Flop
-library ieee;
-use ieee.std_logic_1164.all;
-
-entity flipFlop is
-   port(
-      clock : in std_logic;
-      reset : in std_logic;
-      data : in std_logic;
-      Q : out std_logic
-   );
-end entity flipFlop;
-architecture behavioral of flipFlop is
-  signal qt : std_logic :='1';
-begin
-   process (clock, reset) is
-   begin
-      if (reset = '1') then
-        qt <= '0';
-      elsif clock'event and clock = '1' then
-          qt <= data;
-      end if;
-   end process;
-   Q <= qt;
-end architecture behavioral;
+--library ieee;
+--use ieee.std_logic_1164.all;
+--
+--entity flipFlop is
+--   port(
+--      clock : in std_logic;
+--      reset : in std_logic;
+--      data : in std_logic;
+--      Q : out std_logic
+--   );
+--end entity flipFlop;
+--architecture behavioral of flipFlop is
+--  signal qt : std_logic :='1';
+--begin
+--   process (clock, reset) is
+--   begin
+--      if (reset = '1') then
+--        qt <= '0';
+--      elsif clock'event and clock = '1' then
+--          qt <= data;
+--     end if;
+--   end process;
+--   Q <= qt;
+--end architecture behavioral;
 
 
 
 --D Latch
-library ieee;
-use ieee.std_logic_1164.all;
-
-entity dLatch is
-   port(
-      enable : in std_logic;
-      data : in std_logic;
-      Q : out std_logic
-   );
-end entity dLatch;
-architecture behavioral of dLatch is
-  signal t : std_logic := '0';
-begin
-   with enable select t<=
-    data when '1',
-    t when others;
-   Q <= t;
-end architecture behavioral;
+--library ieee;
+--use ieee.std_logic_1164.all;
+--
+--entity dLatch is
+--   port(
+--      enable : in std_logic;
+--      data : in std_logic;
+--      Q : out std_logic
+--   );
+--end entity dLatch;
+--architecture behavioral of dLatch is
+--  signal t : std_logic := '0';
+--begin
+--   with enable select t<=
+--    data when '1',
+--    t when others;
+--   Q <= t;
+--end architecture behavioral;
