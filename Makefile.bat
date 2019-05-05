68080fa23e2914edc461c98ea71fb1d72ec9ee18
Zmahl <zachmahl@udel.edu>
2019-05-05 17:39:50 -0400

    migrating update2 to master

--- Makefile.bat
+++ Makefile.bat
@@ -1,8 +1,8 @@
-C:\Users\Acer\Documents\Calculator ghdl.exe -a addSub.vhdl
-C:\Users\Acer\Documents\Calculator ghdl.exe -a calculatorTestBench.vhdl
-C:\Users\Acer\Documents\Calculator ghdl.exe -a clockOutput.vhdl
-C:\Users\Acer\Documents\Calculator ghdl.exe -a dLatch.vhdl
-C:\Users\Acer\Documents\Calculator ghdl.exe -a flipFlop.vhdl
-C:\Users\Acer\Documents\Calculator ghdl.exe -a regFile.vhdl
-\U\Users\Acer\Documents\Calculator ghdl.exe -a single_cycle_calc.vhdl
-\\\Users\Acer\Documents\Calculator ghdl.exe -r calculatorTestBench.vhdl
+ghdl -a addSub.vhdl
+ghdl -a calculatorTestBench.vhdl
+ghdl -a clockOutput.vhdl
+ghdl -a dLatch.vhdl
+ghdl -a flipFlop.vhdl
+ghdl -a regFile.vhdl
+ghdl -a single_cycle_calc.vhdl
+ghdl -r calculatorTestBench.vhdl
