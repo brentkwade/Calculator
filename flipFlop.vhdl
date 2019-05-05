93891d61e57836f8bfe16e3624ccdb9f75a53c70
Michael Schwartz <michaelschwartz@Michaels-MacBook-Pro.local>
2019-05-04 20:28:03 -0400

    flip flop done

--- flipFlop.vhdl
+++ flipFlop.vhdl
@@ -4,21 +4,21 @@ use ieee.std_logic_1164.all;
 entity flipFlop is
    port(
       clock : in std_logic;
-      R : in std_logic;
-      D : in std_logic;
+      reset : in std_logic;
+      data : in std_logic;
       Q : out std_logic
    );
 end entity flipFlop;
 architecture behavioral of flipFlop is
   signal qt : std_logic :='1';
 begin
-   process (clock, R) is
+   process (clock, reset) is
    begin
-      if (R = '1') then
+      if (reset = '1') then
         qt <= '0';
       elsif clock'event and clock = '1' then
-          qt <= D;
+          qt <= data;
       end if;
    end process;
-   Q<=qt;
+   Q <= qt;
 end architecture behavioral;
